bcd99a9f6e84f9f0058a9363a1baab53829addf9
Aleksandr Savostin <asavostin1@gmail.com>
2025-06-22 00:09:37 +0500

    WIP

--- src/app/components/client/yandex/YandexMetrika.tsx
+++ src/app/components/client/yandex/YandexMetrika.tsx
@@ -1,4 +1,3 @@
-// components/YandexMetrika.tsx
 import React from "react";
 import Head from "next/head";
 
@@ -19,12 +18,18 @@ const YandexMetrika: React.FC<YandexMetrikaProps> = ({counterId}) => {
                                 (m[i].a=m[i].a||[]).push(arguments)
                             };
                             m[i].l=1*new Date();
+                            for (var j = 0; j < document.scripts.length; j++) {
+                                if (document.scripts[j].src === r) {
+                                    return;
+                                }
+                            }
                             k=e.createElement(t),
                             a=e.getElementsByTagName(t)[0];
-                            k.src=r;
                             k.async=1;
+                            k.src=r;
                             a.parentNode.insertBefore(k,a);
                         })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
                         ym(${counterId}, "init", {
                             clickmap:true,
                             trackLinks:true,
