7d4a4bd9fcc84a13367c86cd25f16d7cfa0dbf74
Aleksandr Savostin <asavostin1@gmail.com>
2025-06-22 00:58:48 +0500

    WIP

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+// next-sitemap.js
+module.exports = {
+    siteUrl: "https://frontend-content.ru", // Укажи здесь свой домен
+    generateRobotsTxt: true, // Генерирует robots.txt файл
+    changefreq: "daily", // Частота изменений сайта
+    priority: 0.7, // Приоритет страниц
+    sitemapSize: 7000, // Максимальное количество URL в одном файле
+};
--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
     output: 'export',
     basePath: '',
     trailingSlash: true,
+    reactStrictMode: true,
   /* config options here */
 };
 
