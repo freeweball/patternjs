8c7893a8825628664804ee9742e5a9ea5ce7fb7d
Aleksandr Savostin <asavostin1@gmail.com>
2025-07-01 01:57:29 +0500

    закрыть меню после клика по ссылке

--- src/app/components/client/simpleNavigationItem/SimpleNavigationItem.tsx
+++ src/app/components/client/simpleNavigationItem/SimpleNavigationItem.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import styles from "./style.module.scss";
 import classNames from "classnames";
 import {useActiveLinkStore} from "@/store/navigation/useActiveLinkStore";
+import {useActiveBurgerStore} from "@/store/header/useActiveBurgerStore";
 
 export type SimpleNavigationItemType = {
     url: string;
@@ -15,7 +16,14 @@ export const SimpleNavigationItem = ({
     text,
 }: SimpleNavigationItemType) => {
     const setActiveLink = useActiveLinkStore((state) => state.setActiveLink);
-    const handleClick = (): void => setActiveLink(url);
+    const activeBurger = useActiveBurgerStore((state) => state.activeBurger);
+    const setActiveBurger = useActiveBurgerStore(
+        (state) => state.setActiveBurger,
+    );
+    const handleClick = (): void => {
+        setActiveLink(url);
+        setActiveBurger(!activeBurger);
+    };
     const textClass = classNames(styles.text, isActive && styles.active);
 
     return (
