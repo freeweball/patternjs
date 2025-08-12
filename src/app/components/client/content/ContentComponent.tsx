f3f366756017a78c89f660bbd297df29474eaf4a
Aleksandr Savostin <asavostin1@gmail.com>
2025-08-12 16:57:06 +0500

    fix h1

--- src/app/components/client/content/ContentComponent.tsx
+++ src/app/components/client/content/ContentComponent.tsx
@@ -2,9 +2,9 @@ import styles from "./style.module.scss";
 
 export const ContentComponent = ({children}: {children: React.ReactNode}) => {
     return (
-        <section className={styles.content}>
+        <div className={styles.content}>
             <div className={styles.header}></div>
             <div className={styles.main}>{children}</div>
-        </section>
+        </div>
     );
 };
